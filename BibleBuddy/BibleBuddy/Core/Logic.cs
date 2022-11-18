ed7fd8a56efea535e8963a6d051d8164df84e913
supaFool <supafool@live.com>
2022-11-18 13:11:07 -0500

    Added Text Handler / Bug Fixes

--- BibleBuddy/BibleBuddy/Core/Logic.cs
+++ BibleBuddy/BibleBuddy/Core/Logic.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using BibleBuddy.Core.BibleVersions;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 
 namespace BibleBuddy.Core
 {
@@ -9,30 +11,33 @@ namespace BibleBuddy.Core
     /// </summary>
     public class Logic
     {
-        private List<BBook> books;
-        public Logic()
+        private BibleBuddyForm _form;
+        public Logic(BibleBuddyForm form)
         {
+            _form = form;
             init();
         }
 
         private void init()
         {
-            books = new List<BBook>();
-            //string file = @File.ReadAllText("C:\\BibleStudyApp\\BibleBuddy\\BibleBuddy\\Core\\Data\\Bible-kjv-master\\1Chronicles.json");
+            Thread thr = new Thread(new ThreadStart(LoadKJV));
+            thr.Start();
+
+        }
+
+        private void LoadKJV()
+        {
+            
+            KJV.KingJamesVersion = new List<BBook>();
 
             foreach (string fileName in Directory.GetFiles("C:\\BibleStudyApp\\BibleBuddy\\BibleBuddy\\Core\\Data\\Bible-kjv-master\\"))
             {
                 string file = @File.ReadAllText(fileName);
-            BBook data = JsonConvert.DeserializeObject<BBook>(file);
+                BBook data = JsonConvert.DeserializeObject<BBook>(file);
                 System.Console.WriteLine($"Adding {data.book}");
-                books.Add(data);
+                KJV.KingJamesVersion.Add(data);
             }
-{
-                // Do something with the file content
-            }
-
-
-
+            System.Console.WriteLine($"Loades {KJV.KingJamesVersion.Count} books.");
         }
     }
 }
---
+++ BibleBuddy/BibleBuddy/Core/TextHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace BibleBuddy.Core
+{
+    using static BibleBuddy.Core.BibleVersions.KJV;
+    internal class TextHandler
+    {
+        private BibleBuddyForm m_form;
+        private string m_name;
+        private string m_chapter;
+        private string m_verse;
+        private string m_verseText;
+
+        public TextHandler(BibleBuddyForm form)
+        {
+               m_form = form;
+        }
+
+        private void GetBookName()
+        {
+        }
+
+        private void SetText()
+        {
+            m_form.Kjv_Label_BookTitle.Text = m_name;
+            m_form.Kjv_Label_Chapter.Text = m_chapter;
+            m_form.Kjv_Label_VerseText.Text = m_verseText;
+            m_form.BibleLayout.Refresh();
+        }
+
+    }
+}
